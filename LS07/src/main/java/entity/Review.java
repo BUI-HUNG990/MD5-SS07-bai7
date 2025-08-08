d3b1e67811568631c8f7437fff351205d7db2ee9
BUI-HUNG990 <hung.buiduc050990@gmail.com>
2025-08-08 09:02:46 +0700

    Add files via upload

--- LS07/src/main/java/entity/Review.java
+++ LS07/src/main/java/entity/Review.java
@@ -17,16 +17,31 @@ public class Review {
     @Size(max = 200, message = "Bình luận không được vượt quá 200 ký tự")
     private String comment;
 
-    // Getters and Setters
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-    public int getRating() { return rating; }
-    public void setRating(int rating) { this.rating = rating; }
+    public int getRating() {
+        return rating;
+    }
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
 
-    public String getComment() { return comment; }
-    public void setComment(String comment) { this.comment = comment; }
+    public String getComment() {
+        return comment;
+    }
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
 }
